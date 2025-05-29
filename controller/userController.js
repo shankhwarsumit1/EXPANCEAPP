5bce9dafa78b38fce7e0bc763a01372a3b1ccf60
shankhwarsumit <shankhwarsumit117@gmail.com>
2025-05-29 18:53:24 +0530

    password encryption

--- controller/userController.js
+++ controller/userController.js
@@ -4,41 +4,61 @@ const bcrypt = require('bcrypt');
 
 
 
-const signup = async(req,res)=>{
-    try{
-      const hashedPassword = await bcrypt.hash(req.body.password,10);
-      const user = await userModel.create({...req.body,
-        password:hashedPassword
+const signup = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password
+    } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await userModel.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+    res.status(201).json({ success: true, message: 'User is added' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false, message: err.message });
+  }
+}
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ where: { email } });
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: 'user not found'
       });
-      res.status(201).send('user is added');
     }
-    catch(err){
-        console.log(err.message);
-        res.status(500).send(err.message);
-    }
-}
 
-const login = async(req,res)=>{
-  try{
-       const {email,password} = req.body;
-       const user = await userModel.findOne({where:{email:email}});
-       if(!user){
-        res.status(404).json({success:false,message:'user not found'});
-       }
-       const isMatch = await bcrypt.compare(password,user.password);
-       if(isMatch){
-        res.status(200).json({success:true,message:"user login successfull"});
-       }
-       else{
-        res.status(401).json({success:false,message:'user not authorized'});
-       }
-  }
-  catch(err){
-console.log(err);
-res.status(500).json({success:false,message:'server error'});
+    const result = await bcrypt.compare(password, user.password);
+    if (result) {
+      res.status(200).json({
+        success: true,
+        message: "user login successfull"
+      });
+    } else {
+      res.status(401).json({
+        success: false,
+        message: 'wrong password'
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: 'server error'
+    });
   }
 }
 
 
 
-module.exports = {signup,login};
+module.exports = {
+  signup,
+  login
+};
