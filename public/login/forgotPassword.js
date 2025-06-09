087eb4b887a8ada8069a03e49c899dcb4985a7d9
shankhwarsumit <shankhwarsumit117@gmail.com>
2025-06-09 16:41:41 +0530

    signup and forgot password frontend styled

--- public/login/forgotPassword.js
+++ public/login/forgotPassword.js
@@ -2,13 +2,17 @@ window.addEventListener('DOMContentLoaded',(e)=>{
     e.preventDefault();
     form = document.getElementById('forgotform');
     const emailInput = document.getElementById('forgot-email');
-    const submitBtn = document.getElementById('submitBtn');
+    const message = document.getElementById('message');
+
     form.addEventListener('submit',async(e)=>{
         try{ e.preventDefault();
+           message.innerText = "Wait for few seconds";
+           message.style.display = "block";
            const obj = {email:emailInput.value};
            const res = await axios.get(`http://localhost:3000/password/forgotpassword`,{
             headers:{"Usermail":obj.email}
            });
+           message.innerText = "Check your email";
            console.log(res.data.code);
         }
         catch(err){
